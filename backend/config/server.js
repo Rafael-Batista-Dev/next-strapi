3dad824b7d66a24816ae8e939a6b67cec82b913e
Rafael-Batist-Dev <dev.rafaelbatista@gmail.com>
2021-02-09 18:47:18 -0300

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '32c3baa87bd9414cd7233681f07e90a0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '15657338-cbe5-4093-87cf-6815aa392634'
+};
